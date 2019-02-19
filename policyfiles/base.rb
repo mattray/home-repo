52d822b62d607662e64e5165757575ee32c6410a
Matt Ray <matthewhray@gmail.com>
2019-02-20 09:52:22 +1100

    Switched to default attributes and added ssh-baseline

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -6,17 +6,19 @@ cookbook 'mattray', git: 'https://github.com/mattray/mattray-cookbook.git'
 
 run_list 'audit', 'chef-client::config', 'chef-client::service', 'chef-client::delete_validation', 'ntp', 'openssh', 'mattray'
 
-override['chef_client']['init_style'] = 'systemd'
-override['chef_client']['interval'] = 1800
-override['chef_client']['splay'] = 100
-override['ntp']['servers'] = ['0.au.pool.ntp.org', '1.au.pool.ntp.org', '2.au.pool.ntp.org', '3.au.pool.ntp.org']
+default['chef_client']['init_style'] = 'systemd'
+default['chef_client']['interval'] = 1800
+default['chef_client']['splay'] = 100
+default['ntp']['servers'] = ['0.au.pool.ntp.org', '1.au.pool.ntp.org', '2.au.pool.ntp.org', '3.au.pool.ntp.org']
 
 # https://docs.chef.io/data_collection_without_server.html
-override['chef_client']['config']['data_collector.server_url'] = 'https://ndnd/data-collector/v0/'
-override['chef_client']['config']['data_collector.token'] = '8ZzgdoqAPRWsW4XOHRiFx7Kbobk='
-override['chef_client']['config']['data_collector.organization'] = 'home'
-override['audit']['reporter'] = 'chef-automate'
-override['audit']['interval']['enabled'] = true
-override['audit']['interval']['time'] = 180 # 8 times a day
+default['chef_client']['config']['data_collector.server_url'] = 'https://ndnd/data-collector/v0/'
+default['chef_client']['config']['data_collector.token'] = '8ZzgdoqAPRWsW4XOHRiFx7Kbobk='
+default['chef_client']['config']['data_collector.organization'] = 'home'
+default['audit']['reporter'] = 'chef-automate'
+default['audit']['interval']['enabled'] = true
+default['audit']['interval']['time'] = 120 # 12 times a day
+default['audit']['inspec_version'] = '3.6.6'
 
-override['audit']['profiles']['linux-patch-baseline'] = { 'url': 'https://github.com/dev-sec/linux-patch-baseline/archive/0.4.0.zip' }
+default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline/archive/0.4.0.zip'
+default['audit']['profiles']['ssh-baseline']['url'] = 'https://github.com/dev-sec/ssh-baseline/archive/2.3.0.tar.gz'
