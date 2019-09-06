21d692d702dcc38e2c48998af64473631e7545dc
Matt Ray <matthewhray@gmail.com>
2019-09-06 10:39:58 +1000

    providing apt-cacher-ng

--- policyfiles/beaglebone.rb
+++ policyfiles/beaglebone.rb
@@ -2,6 +2,9 @@ name 'beaglebone'
 
 include_policy 'base', path: './base.lock.json'
 
-run_list 'mattray::beaglebone'
+default_source :supermarket
+
+run_list 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
 
 override['ohai']['disabled_plugins'] = [ 'dmi' ]
+override['apt']['cacher_dir'] = '/emmc'
