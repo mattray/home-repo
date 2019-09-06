1c4961177721953425810c2a156771cea3f5d834
Matt Ray <matthewhray@gmail.com>
2019-09-06 10:39:31 +1000

    new token, new cacher_client

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -2,7 +2,8 @@ name 'base'
 
 default_source :supermarket
 
-cookbook 'mattray', git: 'https://github.com/mattray/mattray-cookbook.git'
+# cookbook 'mattray', git: 'https://github.com/mattray/mattray-cookbook.git'
+cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
 run_list 'audit', 'chef-client::config', 'chef-client::service', 'chef-client::delete_validation', 'ntp', 'openssh', 'mattray'
 
@@ -12,14 +13,18 @@ default['chef_client']['splay'] = 100
 default['ntp']['servers'] = ['0.au.pool.ntp.org', '1.au.pool.ntp.org', '2.au.pool.ntp.org', '3.au.pool.ntp.org']
 
 # https://docs.chef.io/data_collection_without_server.html
-default['chef_client']['config']['data_collector.server_url'] = 'https://ndnd/data-collector/v0/'
-default['chef_client']['config']['data_collector.token'] = '8ZzgdoqAPRWsW4XOHRiFx7Kbobk='
+default['chef_client']['config']['data_collector.server_url'] = 'https://ndnd.bottlebru.sh/data-collector/v0/'
+default['chef_client']['config']['data_collector.token'] = '35V9X1VO0VRSeUjukPmBsihvwXI='
 default['chef_client']['config']['data_collector.organization'] = 'home'
 default['chef_client']['config']['chef_license'] = 'accept'
 
 default['audit']['reporter'] = 'chef-automate'
 default['audit']['interval']['enabled'] = true
-default['audit']['interval']['time'] = 120 # 12 times a day
+default['audit']['interval']['time'] = 30 # 48 times a day
+
+default['apt']['cacher_client']['cacher_server']['host'] = 'cubert'
+default['apt']['cacher_client']['cacher_server']['port'] = 3142
+default['apt']['cacher_client']['cacher_server']['proxy_ssl'] = true
 
 default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline'
 default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
