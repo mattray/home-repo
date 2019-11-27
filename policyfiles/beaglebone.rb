7fcc9a12338471b1dc2032850501757a7a761f1e
Matt Ray <matthewhray@gmail.com>
2019-11-28 09:30:43 +1100

    Document use of "chef-client -j file.json" passed attributes
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- policyfiles/beaglebone.rb
+++ policyfiles/beaglebone.rb
@@ -4,7 +4,9 @@ include_policy 'base', path: './base.lock.json'
 
 default_source :supermarket
 
-run_list 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
+cookbook 'leds_handler', git: 'https://github.com/mattray/leds_handler-cookbook.git'
+
+run_list 'leds_handler::default', 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
 
 override['ohai']['disabled_plugins'] = [ 'dmi' ]
 override['apt']['cacher_dir'] = '/emmc'
--- policyfiles/rpi.rb
+++ policyfiles/rpi.rb
@@ -4,6 +4,8 @@ include_policy 'base', path: './base.lock.json'
 
 default_source :supermarket
 
-run_list 'mattray::raspberrypi'
+cookbook 'leds_handler', git: 'https://github.com/mattray/leds_handler-cookbook.git'
+
+run_list 'leds_handler::default', 'mattray::raspberrypi'
 
 override['ohai']['disabled_plugins'] = [ 'dmi' ]
