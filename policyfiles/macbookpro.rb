2373ca5f6b5cbce741dc6366eeadab38b9fbf5cc
Matt Ray <github@mattray.dev>
2020-02-12 12:11:33 +1100

    no longer using this

--- policyfiles/macbookpro.rb
+++
@@ -1,9 +0,0 @@
-name 'macbookpro'
-
-include_policy 'x86', path: './x86.lock.json'
-
-default_source :supermarket
-
-cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
-
-run_list 'mattray::macbookpro'
