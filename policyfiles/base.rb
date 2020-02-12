e2e34403ac3cdde9ef70b0886d6d6c29b18d3f01
Matt Ray <github@mattray.dev>
2020-02-13 05:21:45 +1100

    remove ntp (in favor of systemd-timesyncd/chrony) and chef-client::delete_validation (unused). Move to inez for Automate

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -5,15 +5,14 @@ default_source :supermarket
 # cookbook 'mattray', git: 'https://github.com/mattray/mattray-cookbook.git'
 cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
-run_list 'audit', 'chef-client::config', 'chef-client::service', 'chef-client::delete_validation', 'ntp', 'openssh', 'mattray'
+run_list 'audit', 'chef-client::config', 'chef-client::service', 'openssh', 'mattray'
 
 default['chef_client']['init_style'] = 'systemd'
 default['chef_client']['interval'] = 1800
 default['chef_client']['splay'] = 100
-default['ntp']['servers'] = ['0.au.pool.ntp.org', '1.au.pool.ntp.org', '2.au.pool.ntp.org', '3.au.pool.ntp.org']
 
 # https://docs.chef.io/data_collection_without_server.html
-default['chef_client']['config']['data_collector.server_url'] = 'https://ndnd.bottlebru.sh/data-collector/v0/'
+default['chef_client']['config']['data_collector.server_url'] = 'https://inez.bottlebru.sh/data-collector/v0/'
 default['chef_client']['config']['data_collector.token'] = '35V9X1VO0VRSeUjukPmBsihvwXI='
 
 default['chef_client']['config']['chef_license'] = 'accept'
@@ -21,10 +20,6 @@ default['chef_client']['config']['chef_license'] = 'accept'
 default['audit']['reporter'] = 'chef-automate'
 default['audit']['interval']['enabled'] = true
 default['audit']['interval']['time'] = 30 # 48 times a day
-
-default['apt']['cacher_client']['cacher_server']['host'] = 'cubert'
-default['apt']['cacher_client']['cacher_server']['port'] = 3142
-default['apt']['cacher_client']['cacher_server']['proxy_ssl'] = true
 
 default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline'
 default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
