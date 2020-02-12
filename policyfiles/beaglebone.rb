eed85adf1d8c1b63933a4eaa6335c3686865d8fc
Matt Ray <github@mattray.dev>
2020-02-13 05:18:19 +1100

    composite profile of rpi and debian with apt-cacher-ng recipe

--- policyfiles/beaglebone.rb
+++ policyfiles/beaglebone.rb
@@ -1,12 +1,11 @@
 name 'beaglebone'
 
-include_policy 'base', path: './base.lock.json'
-
 default_source :supermarket
 
-cookbook 'leds_handler', git: 'https://github.com/mattray/leds_handler-cookbook.git'
+run_list 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
 
-run_list 'leds_handler::default', 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
+include_policy 'rpi', path: './rpi.lock.json'
+include_policy 'debian', path: './debian.lock.json'
 
 override['ohai']['disabled_plugins'] = [ 'dmi' ]
 override['apt']['cacher_dir'] = '/emmc'
