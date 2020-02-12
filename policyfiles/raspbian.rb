b7bec7519f4a47562c68ead2141b8c91b9ee6832
Matt Ray <github@mattray.dev>
2020-02-13 05:17:15 +1100

    composite profile of rpi and debian

--- policyfiles/raspbian.rb
+++ policyfiles/raspbian.rb
@@ -1,7 +1,9 @@
 name 'raspbian'
 
+include_policy 'debian', path: './debian.lock.json'
 include_policy 'rpi', path: './rpi.lock.json'
 
 default_source :supermarket
+cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
-run_list 'apt::cacher-client'
+run_list 'mattray::raspberrypi'
