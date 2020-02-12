628b74c02c75ad41fe29575a7bbbc23ceb231711
Matt Ray <github@mattray.dev>
2020-02-13 05:20:32 +1100

    use systemd-timesyncd in place of ntp

---
+++ policyfiles/debian.rb
@@ -0,0 +1,15 @@
+name 'debian'
+
+include_policy 'base', path: './base.lock.json'
+
+default_source :supermarket
+
+cookbook 'timesyncd', path: '/Users/mattray/ws/cookbooks/timesyncd'
+
+run_list 'apt::cacher-client', 'timesyncd'
+
+default['apt']['cacher_client']['cacher_server']['host'] = 'cubert'
+default['apt']['cacher_client']['cacher_server']['port'] = 3142
+default['apt']['cacher_client']['cacher_server']['proxy_ssl'] = true
+
+default['timesyncd']['NTP'] = ['10.0.0.1']
