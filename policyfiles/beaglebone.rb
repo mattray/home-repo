0510c14ef7a5f43526eef9b0a6fee9260b6fd8e9
Matt Ray <github@mattray.dev>
2020-02-15 09:24:10 +1100

    not the apt-cacher-ng anymore

--- policyfiles/beaglebone.rb
+++ policyfiles/beaglebone.rb
@@ -1,11 +1,8 @@
 name 'beaglebone'
 
+include_policy 'debian', path: './debian.lock.json'
+include_policy 'rpi', path: './rpi.lock.json'
+
 default_source :supermarket
 
-run_list 'apt::cacher-ng', 'apt::cacher-client', 'mattray::beaglebone'
-
-include_policy 'rpi', path: './rpi.lock.json'
-include_policy 'debian', path: './debian.lock.json'
-
-override['ohai']['disabled_plugins'] = [ 'dmi' ]
-override['apt']['cacher_dir'] = '/emmc'
+run_list 'apt::cacher-client', 'mattray::beaglebone'
