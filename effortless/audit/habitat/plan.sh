e21a850be5b7b4621110be7e3a444dc76991efa1
Matt Ray <github@mattray.dev>
2020-07-06 18:29:25 +1000

    get the latest

--- effortless/audit/habitat/plan.sh
+++ effortless/audit/habitat/plan.sh
@@ -1,4 +1,4 @@
 pkg_name=effortless-ndnd-home
 pkg_version=0.1.8
 pkg_origin=mattray
-pkg_scaffolding="chef/scaffolding-chef-inspec"
+pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200629114059"
