d2a1ba62e6858d9be427b899c1917493ee9d359a
Matt Ray <github@mattray.dev>
2020-07-06 18:17:50 +1000

    Upgrade to chef scaffolding
    Signed-off-by: Matt Ray <github@mattray.dev>

--- effortless/audit/habitat/plan.sh
+++ effortless/audit/habitat/plan.sh
@@ -1,4 +1,4 @@
 pkg_name=effortless-ndnd-home
-pkg_version=0.1.7
+pkg_version=0.1.8
 pkg_origin=mattray
-pkg_scaffolding="mattray/scaffolding-chef-inspec"
+pkg_scaffolding="chef/scaffolding-chef-inspec"
