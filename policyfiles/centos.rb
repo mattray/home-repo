7e95c869c546be835ceb4a64dd1d2b6a01bd0942
Matt Ray <github@mattray.dev>
2020-07-06 23:05:04 +1000

    no longer inherit base

--- policyfiles/centos.rb
+++ policyfiles/centos.rb
@@ -1,12 +1,19 @@
 name 'centos'
 
-include_policy 'base', path: './base.lock.json'
-
 default_source :supermarket
 
 cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
-run_list 'ntp', 'mattray::raspberrypi', 'leds_handler'
+run_list 'audit', 'mattray', 'chef-client::config', 'openssh', 'ntp', 'mattray::raspberrypi', 'leds_handler'
+
+default['audit']['reporter'] = 'chef-server-automate'
+default['audit']['fetcher'] = 'chef-server'
+default['audit']['interval']['enabled'] = true
+default['audit']['interval']['time'] = 30 # 48 times a day
+default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/mattray/linux-patch-baseline/tree/debian_fix'
+default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
+
+default['chef_client']['config']['chef_license'] = 'accept'
 
 default['ntp']['servers'] = ['10.0.0.1']
 
