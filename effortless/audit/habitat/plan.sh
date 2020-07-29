3d946adbb07758a8255cc51f7ac8e8fa7a361bc9
Matt Ray <github@mattray.dev>
2020-07-29 16:58:45 +1000

    Update to use dev-sec.io's linux-patch-baseline
    Signed-off-by: Matt Ray <github@mattray.dev>

--- effortless/audit/habitat/plan.sh
+++ effortless/audit/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=effortless-ndnd-home
 pkg_origin=mattray
-pkg_version=0.1.8
+pkg_version=0.1.9
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
-pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200629114059"
+pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200707104227"
