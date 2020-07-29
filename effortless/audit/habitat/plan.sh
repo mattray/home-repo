c7991c0f103d6cb2e57c46f58796d972c9f52906
Matt Ray <github@mattray.dev>
2020-07-29 18:00:46 +1000

    get the right version of the scaffolding
    Signed-off-by: Matt Ray <github@mattray.dev>

--- effortless/audit/habitat/plan.sh
+++ effortless/audit/habitat/plan.sh
@@ -3,4 +3,4 @@ pkg_origin=mattray
 pkg_version=0.1.9
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
-pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200707104227"
+pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200707104154"
