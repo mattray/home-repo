b2d4c0085e3edb46ca12fcd30ccc339ae0908155
Matt Ray <github@mattray.dev>
2020-08-21 16:19:25 +1000

    switch to upstream dev-sec profile

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -10,7 +10,7 @@ default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['fetcher'] = 'chef-server'
 default['audit']['interval']['enabled'] = true
 default['audit']['interval']['time'] = 30 # 48 times a day
-default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/mattray/linux-patch-baseline/tree/debian_fix'
+default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline'
 default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
 
 default['chef_client']['config']['chef_license'] = 'accept'
