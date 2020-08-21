0bd59e4b6300cb636dc52d66b233fa30b2771b27
Matt Ray <github@mattray.dev>
2020-08-21 16:19:01 +1000

    switch to upstream dev-sec profile, try to disable shard_seed

--- policyfiles/centos.rb
+++ policyfiles/centos.rb
@@ -10,11 +10,11 @@ default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['fetcher'] = 'chef-server'
 default['audit']['interval']['enabled'] = true
 default['audit']['interval']['time'] = 30 # 48 times a day
-default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/mattray/linux-patch-baseline/tree/debian_fix'
+default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline'
 default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
 
 default['chef_client']['config']['chef_license'] = 'accept'
 
 default['ntp']['servers'] = ['10.0.0.1']
 
-override['ohai']['disabled_plugins'] = [ 'dmi' ]
+override['ohai']['disabled_plugins'] = [ 'dmi', 'shard_seed' ]
