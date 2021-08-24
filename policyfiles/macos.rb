796fdd34ecfb3903f5d41f33459fa1baaf19ab2d
Matt Ray <github@mattray.dev>
2021-08-24 17:28:22 +1000

    removed Audit

--- policyfiles/macos.rb
+++ policyfiles/macos.rb
@@ -4,12 +4,6 @@ default_source :supermarket
 
 cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
-run_list 'audit', 'chef-client::config', 'chef-client::service', 'mattray', 'chef_client_updater', 'mattray::macos'
+run_list 'mattray', 'mattray::macos', 'chef-client::config', 'chef-client::service'
 
-default['chef_client']['config']['chef_license'] = 'accept'
-
-default['audit']['reporter'] = 'chef-server-automate'
-default['audit']['fetcher'] = 'chef-server'
-default['audit']['interval']['enabled'] = true
-default['audit']['interval']['time'] = 60 # 24 times a day
-default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
+default['chef_client']['config']['log_location'] = '/tmp/chef.log'
