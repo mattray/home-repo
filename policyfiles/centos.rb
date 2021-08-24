7a520962fbb473577d5f21c87cf19e7fea2bd711
Matt Ray <github@mattray.dev>
2021-08-24 17:27:49 +1000

    removed Audit, unused for now

--- policyfiles/centos.rb
+++ policyfiles/centos.rb
@@ -4,16 +4,7 @@ default_source :supermarket
 
 cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
 
-run_list 'audit', 'mattray', 'chef-client::config', 'openssh', 'ntp', 'mattray::raspberrypi', 'leds_handler'
-
-default['audit']['reporter'] = 'chef-server-automate'
-default['audit']['fetcher'] = 'chef-server'
-default['audit']['interval']['enabled'] = true
-default['audit']['interval']['time'] = 30 # 48 times a day
-default['audit']['profiles']['linux-patch-baseline']['url'] = 'https://github.com/dev-sec/linux-patch-baseline'
-default['audit']['profiles']['uptime']['url'] = 'https://github.com/mattray/uptime-profile/'
-
-default['chef_client']['config']['chef_license'] = 'accept'
+run_list 'mattray', 'mattray::raspberrypi', 'chef-client::config', 'openssh', 'ntp', 'leds_handler'
 
 default['ntp']['servers'] = ['10.0.0.1']
 
