86302af866d45d512967561c7b84a62a0a3f4f54
Matt Ray <github@mattray.dev>
2021-08-24 17:29:41 +1000

    added starship. removed Debian

--- policyfiles/workstation.rb
+++ policyfiles/workstation.rb
@@ -1,15 +1,11 @@
 name 'workstation'
 
-include_policy 'effortless', path: './effortless.lock.json'
-
 default_source :supermarket
 
 cookbook 'mattray', path: '/Users/mattray/ws/cookbooks/mattray'
+cookbook 'starship', path: '/Users/mattray/ws/cookbooks/starship'
+cookbook 'systemd_timesyncd', path: '/Users/mattray/ws/cookbooks/systemd_timesyncd'
 
-run_list 'mattray', 'chef-client::config', 'openssh', 'apt::cacher-client', 'chef_client_updater'
+run_list 'mattray', 'chef-client::config', 'openssh', 'systemd_timesyncd', 'starship'
 
-default['chef_client']['config']['chef_license'] = 'accept'
-
-default['apt']['cacher_client']['cacher_server']['host'] = 'cubert'
-default['apt']['cacher_client']['cacher_server']['port'] = 3142
-default['apt']['cacher_client']['cacher_server']['proxy_ssl'] = true
+default['systemd_timesyncd']['ntp'] = ['10.0.0.1']
